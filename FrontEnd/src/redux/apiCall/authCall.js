7b8461e28855da9ddf3cdb3d9a0ca28690bd725c
Ahmed Mehana <102672897+ahmed7mehana@users.noreply.github.com>
2023-08-29 11:25:36 +0300

    Update authCall.js

--- FrontEnd/src/redux/apiCall/authCall.js
+++ FrontEnd/src/redux/apiCall/authCall.js
@@ -6,7 +6,7 @@ import { authActions } from "../authSlice";
 export function loginUser(user) {
     return async (dispatch) => {
       try {
-        const { data } = await request.post("/api/auth/login",user);
+      const { data } = await request.post("/api/auth/login",user);
         dispatch(authActions.login(data));
         localStorage.setItem("userInfo", JSON.stringify(data));
       } catch (error) {
